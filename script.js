bff344c5e58e6d4731e475fa9fce481c6f878d6f
mahjabin2024 <mahjabin.mahjabin@opetus.tampere.fi>
2024-11-12 23:24:48 +0200

    firstjs

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("js").innerHTML = "Javascript"
+//document.getElementsByClassName("para").innerHTML = "This is my paragraph"
+
+var number1 = 10
+var number2 = 20
+var sum = number1 + number2
+var sub = number1 - number2
+var mul = number1 * number2
+var div = number1 / number2
+var rem = number1 % number2
+document.getElementById("sum").innerHTML = number1 + " + " + number2 + " is " + sum
+document.getElementById("sub").innerHTML = number1 + " - " + number2 + " is " + sub
+document.getElementById("mul").innerHTML = number1 + " * " + number2 + " is " + mul
+document.getElementById("div").innerHTML = number1 + " / " + number2 + " is " + div
+document.getElementById("rem").innerHTML = number1 + " % " + number2 + " is " + rem
